5796d20945dd19ae94041602393d7b7731c77021
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-01-20 17:56:38 +0300

    Bump wheel version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 setup(
     author='Emelyanenko Kirill',
     name='pyTON',
-    version='0.1.2',
+    version='0.1.3',
     packages=['pyTON'],
     install_requires=requirements,
     package_data={
